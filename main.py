b4b6be327a56c0efdef790ead544aafbcc916e12
Debashrita Mandal <mandaldebashrita@gmail.com>
2026-01-16 23:31:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #code here
 under construction
+#under preview
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
